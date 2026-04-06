f5d682e6a4d4e320450e78504f52453f41bcb2ee
Richard Leddy <richardaleddy@gmail.com>
2026-04-06 13:21:59 -0700

    css turns out to be a little bit more hand work
    automation has helped by making inputs such as variable collections, etc.

---
+++ tools/css_factor_best_choices.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+let best_margin_control = `
+*, *::before, *::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+`
+
+
+let prefered_html_defaults = `
+html {
+    height : 100%
+}
+`
+
